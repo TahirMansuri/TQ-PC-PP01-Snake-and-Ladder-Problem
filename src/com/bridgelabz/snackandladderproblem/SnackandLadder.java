5d7a5781e34ebe46b73af40de9f1a5becb4b088c
TahirMansuri <tprocks786@gmail.com>
2021-12-31 14:44:39 +0530

    MODIFIED: Set Player Initial Position

--- src/com/bridgelabz/snackandladderproblem/SnackandLadder.java
+++ src/com/bridgelabz/snackandladderproblem/SnackandLadder.java
@@ -1,8 +1,23 @@
 package com.bridgelabz.snackandladderproblem;
 
 public class SnackandLadder {
+
+    //Declaring Variables for Players Position
+    int playerPosition = 0;
+
+    //Method to Show Player Current Position
+    private void showPosition() {
+        //Displaying Player Position
+        System.out.println("Player Starting Position = "+ playerPosition);
+    }
     public static void main(String[] args) {
         //Displaying Welcome Message
         System.out.println("Welcome to Snake and Ladder Simulator Problem Developed by Tahir Mansuri.");
+
+        //Creating Object for Player
+        SnackandLadder player1 = new SnackandLadder();
+
+        //Showing Player Position
+        player1.showPosition();
     }
 }
