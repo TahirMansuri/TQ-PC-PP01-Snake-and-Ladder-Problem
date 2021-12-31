94f5d67773219ad8c5896b774ad1e3613c57a93f
TahirMansuri <tprocks786@gmail.com>
2021-12-31 13:46:48 +0530

    ADDED: Snake and Ladder Simulator Problem

---
+++ src/com/bridgelabz/snackandladderproblem/SnackandLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.snackandladderproblem;
+
+public class SnackandLadder {
+    public static void main(String[] args) {
+        //Displaying Welcome Message
+        System.out.println("Welcome to Snake and Ladder Simulator Problem Developed by Tahir Mansuri.");
+    }
+}
