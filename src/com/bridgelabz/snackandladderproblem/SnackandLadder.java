8f8e78d73eaf388738d55d49323a381c7f553713
TahirMansuri <tprocks786@gmail.com>
2021-12-31 17:24:43 +0530

    MODIFIED: Reporting the Dice Count and Position of Player

--- src/com/bridgelabz/snackandladderproblem/SnackandLadder.java
+++ src/com/bridgelabz/snackandladderproblem/SnackandLadder.java
@@ -11,7 +11,7 @@ public class SnackandLadder {
 
     //Declaring Variables for Players Position
     int playerPosition = 0;
-    int newPosition = 0;
+    int diceCount = 0;
 
     // Random Class Object for Generating Random Numbers
     Random randomNo = new Random();
@@ -25,6 +25,11 @@ public class SnackandLadder {
     //Method to Roll the Die and Get No between 1 to 6
     private int rollDie() {
         int dieNo = randomNo.nextInt(6)+1;
+
+        //Incrementing the Dice Count on each die roll
+        diceCount++;
+        System.out.println("Dice Count : "+diceCount);
+
         return dieNo;
     }
 
